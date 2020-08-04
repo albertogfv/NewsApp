f556e1cbba5f80d6e6ffe85d244e46c85d520452
albertogfv <albertogfv@gmail.com>
2020-08-04 07:19:07 -0700

    refactoring

---
+++ app/src/main/java/com/albertogfv/newsapp/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.albertogfv.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+    }
+}
