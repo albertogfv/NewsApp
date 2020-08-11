5d55e0b2589dd2f94296f527ea4c02fc79f9bed4
albertogfv <albertogfv@gmail.com>
2020-08-10 20:35:56 -0700

    added files

--- app/src/main/java/com/albertogfv/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/albertogfv/newsapp/ui/NewsActivity.kt
@@ -2,12 +2,17 @@ package com.albertogfv.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.albertogfv.newsapp.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        bottomNavigatonView.setupWithNavController(newsNavHostFragment.findNavController())
+
     }
-}
+} 
